c26b05b52634e3251c74a8bda7e834b036955922
Andy-K-Sparklight <Twilight_Princess_Sparkle@outlook.com>
2021-08-20 09:01:41 +0800

    Site Update

--- pub.sh
+++ pub.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd `dirname $0`
+hexo cl
 git add .
 git commit -m "Site Update"
 git push -u origin main
+hexo d
---
+++ source/_posts/release-crystal.md
@@ -0,0 +1,6 @@
+---
+title: Alicorn Crystal 发行版
+date: 2021-08-18 16:00:19
+categories:
+  - 发行版
+---
